07f3883ed56717153891aa6c2245e71fb3e60ed0
HENRY-2016 <jacob2016henry@gmail.com>
2021-03-18 17:10:09 +0300

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,53 +1,12 @@
 
-// 192.168.43.23
 
-
-function CreateCustomAlertBox()
+// Esther
+function LoadKankanApp ()
 {
-    this.render = function(dialog){
-        let winW = window.innerWidth;
-        let winH = window.innerHeight;
-        let AlertBoxDisplayArea = document.getElementById('alert-box-display-area');
-        let HtmlAlertBox = document.getElementById('alert-box');
-        AlertBoxDisplayArea.style.display = "block";
-        AlertBoxDisplayArea.style.height = winH+"px";
-        HtmlAlertBox.style.left = (winW/2) - (550 * .5)+"px";
-        HtmlAlertBox.style.top = "100px";
-        HtmlAlertBox.style.display = "block";
-        document.getElementById('alert-box-header').innerHTML = "Network Or Server Error ";
-        document.getElementById('alert-box-body').innerHTML = dialog;
-        document.getElementById('alert-box-footer').innerHTML = '<button class="ok-alert-btn" onclick="Alert.ok()">OK</button>';
-    }
-    this.ok = function()
-        {
-            document.getElementById('alert-box').style.display = "none";
-            document.getElementById('alert-box-display-area').style.display = "none";
-        }
-}
-var Alert = new CreateCustomAlertBox();
+window.location='http://178.79.182.213/kankan-php/authenticate.php'
+// window.location='http://localhost/kankan-php/authenticate.php'
 
-function ShowAlertBox (){Alert.render('<center>Please turn on <br> your network <br>and <br> Try Again</center>')}
-
-
-// Adonoi
-function LoadAdonoiUsingGitHub ()
-{
-    if(!navigator.onLine)
-    {
-        CreateCustomAlertBox ()
-    }
-    
-    if(navigator.onLine)
-    {window.location='https://henry-2016.github.io/adonai-client-server-repo/'}
     
 }
 
-function LoadAdonoiUsingRaspberry ()
-
-{
-    if(!navigator.onLine)
-        {CreateCustomAlertBox ()}
-    if(navigator.onLine)
-        {window.location="http://192.168.43.140:80/adonai-client-server-repo/index.html"}
-}
 
